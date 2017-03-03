1251bd03a79f814d2c4b19806f1b707037e34155
Sam Kling <samtkling@gmail.com>
2017-03-03 11:02:25 -0600

    added some initial styling and details

--- index.php
+++ index.php
@@ -12,14 +12,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to Sam Kling's Homepage</title>
+    <title>Sam Kling's Homepage</title>
     <link href="style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-
 <header>
-    <p>Hello World</p>
+    <p class="title">Sam Kling</p>
 </header>
+
+<div>
+    <p class="welcome">Welcome to the personal homepage of Sam Kling. This page is to be updated continuously as things
+    in his life change. Please look forward to the addition of more content and styling
+    practices as this site will also be used as a way to showcase his web development skills.
+    </p>
+</div>
+
+<div class="resume">
+    <iframe src="SamKlingResume.pdf">
+</div>
 
 
 </body>
