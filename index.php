c1bb33945dc9e52bbd926017b5ec23b9868526d3
Sam Kling <samtkling@gmail.com>
2017-03-03 01:38:02 -0500

    initial page creation

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samkling
+ * Date: 3/3/17
+ * Time: 1:29 AM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Sam Kling's Homepage</title>
+    <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <p>Hello World</p>
+</header>
+
+
+</body>
+</html>
